f77497ea67d91a2fc7e44ba55e47c22eeca5bfc3
Pasinduthennakoon <pasinduthennakoon512@gmail.com>
2024-10-21 10:40:32 +0530

    import express

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/",(req,res) => {
+    res.send("<h1>home page</h1>")
+});
+
+app.listen(5000, () => {
+    console.log("server started on port 5000");
+})
