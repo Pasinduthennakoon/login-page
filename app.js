35ae0e750448f01ccc463fdf495b7f0157f84a42
Pasinduthennakoon <pasinduthennakoon512@gmail.com>
2024-10-21 11:26:39 +0530

    set to goto index page

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const mysql = require("mysql");
 const dotenv = require("dotenv");
 
@@ -12,6 +13,10 @@ const db = mysql.createConnection({
     database: process.env.DATABASE
 });
 
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory));
+app.set('view engine','hbs'); 
+
 db.connect( (error) =>{
     if(error){
         console.log(error)
@@ -21,7 +26,7 @@ db.connect( (error) =>{
 })
 
 app.get("/",(req,res) => {
-    res.send("<h1>home page</h1>")
+    res.render("index")
 });
 
 app.listen(5000, () => {
